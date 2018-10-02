860f2dee979bd6ae157408def0e54f8ccd9991cc
Keith Winfield <keith.r.winfield@gmail.com>
2018-10-02 12:52:47 -0400

    Updated Comic Book Detail View Layout

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,7 +16,7 @@ namespace ComicBookGallery.Controllers
             {
                 SeriesTitle = "The Amazing Spider-man",
                 IssueNumber = 700,
-                DescriptionHTML = "<p> Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                DescriptionHTML = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                     {
                         new Artist() { Name = "Dan Slott", Role = "Script"},
